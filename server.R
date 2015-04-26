fb773a88ce25f7be5821acf1696dfe8295297872
Rafael Espericueta <rafaelespericueta@gmail.com>
2015-04-26 11:29:18 -0700

    upload shiny app

---
+++ server.R
@@ -0,0 +1,48 @@
+#
+# server.R (for a shiny app)
+#
+# Developing Data Products Course Project
+# by Rafael Espericueta
+
+library(MASS)
+library(ggplot2)
+
+function(input, output, session) {
+  # Define a reactive expression for the document term matrix
+  terms <- reactive({
+    # Change when the "update" button is pressed...
+    input$N
+    input$Sx
+    input$Sy
+    input$r
+  })
+  
+  output$plot <- renderPlot({
+    v <- terms()
+    
+    sxy <- input$r * input$Sx * input$Sy
+    Sigma <- matrix(c(input$Sx^2, sxy, sxy, input$Sy^2), 2, 2)
+    df <- data.frame(mvrnorm(n = input$N, rep(0, 2), Sigma, empirical = TRUE))
+    colnames(df) <- c('x', 'y')
+    
+    # Find the max and min x and y coordinate to create a viewing square.
+    xmin <- min(df$x);  xmax <- max(df$x)
+    ymin <- min(df$y);  ymax <- max(df$y)
+    square_radius <- max(abs(xmin), abs(xmax), abs(ymin), abs(ymax))
+    xmin <- -square_radius;  xmax <- square_radius
+    ymin <- -square_radius;  ymax <- square_radius
+    
+    # 2D plot of sample of bivariate random variables, 
+    # along with its regression line.
+    p <- ggplot(df, aes(x, y))  +
+      geom_point(colour = "skyblue", size = 5) + 
+      geom_point(alpha = .1) +
+      xlim(xmin, xmax) + ylim(ymin, ymax) +
+      geom_smooth(method = lm,   # Add linear regression line
+                  color = 'red')
+    print(p)
+  })
+}
+
+
+
---
+++ ui.R
@@ -0,0 +1,38 @@
+#
+# ui.R (for a shiny app)
+#
+# Developing Data Products Course Project
+# by Rafael Espericueta
+
+fluidPage(
+  # Application title
+  titlePanel("Bivariate Normal Distribution Explorer"),
+  
+  sidebarLayout(
+    # Sidebar with a slider and selection inputs
+    sidebarPanel(
+      sliderInput("N",
+                  "Number samples:",
+                  min = 3,  max = 10000, value = 500),
+      sliderInput("Sx",
+                  "Standard deviation for x:",
+                  min = 1,  max = 30,  value = 5),
+      sliderInput("Sy",
+                  "Standard deviation for y:",
+                  min = 1,  max = 30,  value = 3),
+      sliderInput("r",
+                  "Correlation of x and y:",
+                  min = 0,  max = 0.99,  value = 0.5),
+      hr(),
+      helpText("Use the above sliders to set the number of samples N, the standard deviations Sx and Sy, and the correlation of x and y. "),
+      helpText("The result: a plot of a bivariate normal sample chosen from the specified distribution."),
+      hr(),
+      helpText("Github Repo: http://this will be a valid address!")
+    ),
+    
+    # Show Bivariate Distribution
+    mainPanel(
+      plotOutput("plot")
+    )
+  )
+)
